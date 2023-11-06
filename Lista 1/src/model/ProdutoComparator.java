cb28a26ef29c7731bc0c224546bde4d8c42b1308
Mono-Rim <113648099+Mono-Rim@users.noreply.github.com>
2023-11-06 20:41:30 -0300

    Delete Lista 1 directory

--- Lista 1/src/model/ProdutoComparator.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class ProdutoComparator implements java.util.Comparator<Produto>{
-    @Override
-    public int compare(Produto a, Produto b) {
-        return a.getId() - b.getId();
-    }
-}
