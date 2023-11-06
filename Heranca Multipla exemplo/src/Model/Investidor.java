96475697244062864ba05849445ed8b55a57c16c
Mono-Rim <113648099+Mono-Rim@users.noreply.github.com>
2023-11-06 20:41:22 -0300

    Delete Heranca Multipla exemplo directory

--- Heranca Multipla exemplo/src/Model/Investidor.java
+++
@@ -1,8 +0,0 @@
-package Model;
-
-public interface Investidor {
-     String getTicker();
-     void setTicker(String Ticker);
-     int getQuantidade();
-     void setQuantidade(int quantidade);
-}
