42f1de1b84eb5baac9eefb84fcad1db5c15edb61
Mono-Rim <113648099+Mono-Rim@users.noreply.github.com>
2023-11-06 20:41:02 -0300

    Delete Avaliacao 1 directory

--- Avaliacao 1/src/model/ProdutoComparator.java
+++
@@ -1,8 +0,0 @@
-package model;
-
-public class ProdutoComparator implements java.util.Comparator<Aluno>{
-    @Override
-    public int compare(Aluno a, Aluno b) {
-        return a.getId() - b.getId();
-    }
-}
